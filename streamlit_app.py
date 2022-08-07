49188576a627c5169145b8d00a08251269db89f7
SriniSuni <110736592+SriniSuni@users.noreply.github.com>
2022-08-07 09:17:17 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text("Bread and Milk🥛")
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 df = streamlit.dataframe(my_fruit_list)
-df
+steamlit.df
 
 
 
