fd4270ebf836027a63ea018155f4fc994451ac02
SriniSuni <110736592+SriniSuni@users.noreply.github.com>
2022-08-07 07:57:28 +0400

    Update streamlit_app.py
    Import Pandas
    Load csv into data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.header("Breakfast Menu✍")
 streamlit.text(" Oatmeal")
 streamlit.text("salads🥗🥛")
 streamlit.text("Bread and Milk🥛")
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
