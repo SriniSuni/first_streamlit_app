fa2b8dbd4501da090cf03066e07dafa30b86e5eb
SriniSuni <110736592+SriniSuni@users.noreply.github.com>
2022-08-07 08:48:13 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,6 @@ streamlit.dataframe(my_fruit_list)
 
 steamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
 steamlit.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
