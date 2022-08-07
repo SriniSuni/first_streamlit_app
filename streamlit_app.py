db421bafc05c84970463b624bc6fe32cce63eca4
SriniSuni <110736592+SriniSuni@users.noreply.github.com>
2022-08-07 08:47:20 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
-steamlit.multiselect("pick some fruits:" list(my_fruit_list.index))
+steamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
 steamlit.dataframe(my_fruit_list)
